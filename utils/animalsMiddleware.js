72fdd96304eb0ee3b7b4977bd04bf31be3cd01f9
mightyhamsterlord007 <pak-code-teacher@jd-instructor.local>
2018-07-25 12:17:17 -0400

    add find duplicate middleware

--- utils/animalsMiddleware.js
+++ utils/animalsMiddleware.js
@@ -1,12 +1,34 @@
+var Animal = require('../models/animal');
+
 module.exports = {
     makeSound: function(req, res, next) {
         console.log('HELLO CLASSSSSS');
         next();
     },
     checkIfAnimalNameExist: function(req, res, next) {
-        console.log('Animal Middleware');
-        console.log('---------------')
-        console.log(req.body)
-        next();
+        
+        const name = req.body.name;
+
+        Animal.findOne({name: name}, function(err, result) {
+
+            if (err) {
+                res.status(500).json({
+                    message: 'Fail',
+                    data: err
+                });
+                return;
+            }
+
+            if (result === null) {
+                next();
+            } else {
+                console.log(result)
+                res.json({
+                    message: 'User already exist',
+                    data: result
+                });
+            }
+        });
+
     }
 }
